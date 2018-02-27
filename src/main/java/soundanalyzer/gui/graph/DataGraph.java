e7e6d27463efce5a7bd9208f7c5e2e38717e7d73
Marc-Antoine Dumais <mdumais@expedia.com>
2018-02-27 13:17:31 -0500

    fix graph and add log

--- src/main/java/soundanalyzer/gui/graph/DataGraph.java
+++ src/main/java/soundanalyzer/gui/graph/DataGraph.java
@@ -7,16 +7,15 @@ import soundanalyzer.config.ApplicationContextProvider;
 import soundanalyzer.config.AudioConfig;
 import soundanalyzer.model.SinWave;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.stream.Collectors;
 
 public class DataGraph extends JPanel {
-    private static final long serialVersionUID = -2423472748202716661L;
-
-    private int width, xStart, xEnd, xSize,
-    height, yStart, yEnd, ySize;
+    private static final long serialVersionUID = -2426472248202716611L;
 
     private double[][] data;
 
@@ -24,62 +23,25 @@ public class DataGraph extends JPanel {
         setOpaque(true);
         setPreferredSize(new Dimension(400, 200));
         data = new double[][]{};
-        recalculatePositions();
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        recalculatePositions();
         Graphics2D g2d = (Graphics2D)g;
 
         g2d.setColor(Color.WHITE);
-        g2d.fillRect(0, 0, width, height);
-
-        // Axis drawing
-        g2d.setColor(Color.BLACK);
-        g2d.setStroke(new BasicStroke(2));
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setFont(new Font("Serif", Font.PLAIN, 12));
-        FontMetrics fm = g2d.getFontMetrics();
-
-        // X-axis
-        g2d.drawLine(xStart, yEnd, xEnd, yEnd);
-        if (data.length > 0) {
-            int maxSample = data.length;
-            int xStep = maxSample / 10;
-            for (int sample = xStep; sample <= maxSample; sample += xStep) {
-                int x = xStart + sample * xSize / maxSample;
-                g2d.drawLine(x, yEnd + 1, x, yEnd - 1);
-                String text = "" + sample;
-                g2d.drawString(text,
-                        x - fm.stringWidth(text) / 2,
-                        height - 2);
-            }
-        }
-
-        // Y-axis
-        g2d.drawLine(xStart, yStart, xStart, yEnd);
-        if (data.length > 0) {
-            int maxHeight = data[0].length;
-            int yStep = maxHeight / 10;
-            for (int val = yStep; val <= maxHeight; val += yStep) {
-                int y = yEnd + yStep - val * ySize / maxHeight;
-                g2d.drawLine(xStart - 1, y, xStart + 1, y);
-                String text = "" + val;
-                g2d.drawString(text,
-                        xStart - fm.stringWidth(text) - 2,
-                        y - fm.getHeight());
-            }
-        }
+        g2d.fillRect(0, 0, getWidth(), getHeight());
 
         // Points drawing
         if (data.length > 0) {
-            int pWidth = xSize / data.length;
-            int pHeight = ySize / data[0].length;
+            double xStep = (double)(getWidth()) / data.length;
+            double yStep = (double)(getHeight()) / data[0].length;
+            int pWidth = Math.max(1, (int)xStep);
+            int pHeight = Math.max(1, (int)yStep);
             for (int i = 0; i < data.length; i++) {
                 for (int j = 0; j < data[0].length; j++) {
                     g2d.setColor(ColorPalettes.viridis(data[i][j]));
-                    g2d.fillRect(xStart + pWidth * i, yEnd - pHeight * j, pWidth, pHeight);
+                    g2d.fillRect((int)(xStep * i), (int)(getHeight() - yStep * j), pWidth + 1, pHeight + 1);
                 }
             }
         }
@@ -106,21 +68,13 @@ public class DataGraph extends JPanel {
                 freqs = analyzerService.fourierTransform(frameData[i], false);
                 for (int j = 0; j < data[0].length; j++) {
                     data[i][j] = freqs.get(j).amplitude;
+                    if (data[i][j] > 0) {
+                        data[i][j] = Math.log(data[i][j]);
+                    }
                 }
             }
         }
-        
-        repaint();
-    }
 
-    public void recalculatePositions() {
-        width = getWidth();
-        height = getHeight();
-        xStart = 20;
-        xEnd = width - 20;
-        xSize = xEnd - xStart;
-        yStart = 10;
-        yEnd = height - 16;
-        ySize = yEnd - yStart;
+        repaint();
     }
 }
